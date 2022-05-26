fdf5d8d752a21792997e623c1c2a9248596aa980
Alexis avila R <84880348+Alexisrodri@users.noreply.github.com>
2022-05-26 12:37:03 -0500

    Add files via upload
    VS1

---
+++ main.js
@@ -0,0 +1,47 @@
+
+const previous = document.getElementById("previous")
+const play = document.getElementById("play")
+const pause = document.getElementById('pause')
+const next = document.getElementById("next")
+const musicName = document.getElementById('Title')
+const musicArtist = document.getElementById('Artist')
+const musicImg = document.getElementById('Image')
+const Audio = document.getElementById('Audio')
+
+
+let indexMusic =1;
+
+window.addEventListener('load',()=>{
+    loadMusic(indexMusic);
+})
+
+function loadMusic(indexNumb){
+    musicName.innerText = playList[indexNumb - 1].name
+    musicArtist.innerText = playList[indexNumb - 1].artist
+    musicImg.src = `multimedia/${playList[indexNumb - 1].img}.png`
+    Audio.src = `multimedia/${playList[indexNumb - 1].src}.mp3`
+}
+
+play.addEventListener("click",()=>{
+    play.classList.toggle("Play");
+    if(Audio.paused || Audio.ended) {
+        Audio.play()
+    }else{
+        Audio.pause();
+    }
+})
+
+next.addEventListener('click',()=>{
+     indexMusic++;
+     indexMusic > playList.length ? indexMusic = 1 : indexMusic = indexMusic 
+     loadMusic(indexMusic);
+     Audio.play()
+})
+
+
+previous.addEventListener('click',()=>{
+    indexMusic--;
+    indexMusic < 1 ? indexMusic = playList.length : indexMusic = indexMusic 
+    loadMusic(indexMusic);
+    Audio.play()
+})
---
+++ multimedia/Music.js
@@ -0,0 +1,32 @@
+const playList = [
+    {
+        name:"Cannon Rock",
+        artist:"Sungha jung",
+        img:"Images/img-1",
+        src:"./Musica/cannonRock"
+    },
+    {
+        name:"Wake me up when september ends",
+        artist:"Green Day",
+        img:"./Images/img-2",
+        src:"./Musica/Greenday"
+    },
+    {
+        name:"I Hate Everything About you",
+        artist:"Three days grace",
+        img:"./Images/img-3",
+        src:"./Musica/IhateEverething"
+    },
+    {
+        name:"Teenagers",
+        artist:"MCR",
+        img:"./Images/img-4",
+        src:"./Musica/TeenagersMCR"
+    },
+    {
+        name:"Under The Bridge",
+        artist:"RHCP",
+        img:"./Images/img-5",
+        src:"./Musica/Underthebridge"
+    },
+]
